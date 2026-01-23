91b12d8051437f63c37c5de9917f610ec0bb4b38
Wouter van Zeist <65263211+wvanzeist@users.noreply.github.com>
2026-01-23 14:38:32 +0100

    Don't overwrite full galaxy files before running downsampled

--- test_Galaxy_class.py
+++ test_Galaxy_class.py
@@ -6,7 +6,7 @@ import argparse
 ModelParams = { #Main options
                'GalaxyModel': 'Besancon', #Currently can only be Besancon
                'RecalculateNormConstants': True, #If true, density normalisations are recalculated and printed out, else already existing versions are used
-               'RecalculateCDFs': True, #If true, the galaxy distribution CDFs are recalculated (use True when running first time on a new machine)
+               'RecalculateCDFs': False, #If true, the galaxy distribution CDFs are recalculated (use True when running first time on a new machine)
                'ImportSimulation': True, #If true, construct the present-day DWD populaiton (as opposed to the MS population)               
                #Simulation options
                'RunWave': 'initial_condition_variations',
@@ -28,7 +28,7 @@ ModelParams = { #Main options
                #'Code': 'COMPAS',
                #Simulation parameters
                'ACutRSunPre': 6., #Initial cut for all DWD binaries
-               'UseRepresentingWDs': False, #If False - each binary in the Galaxy is drawn as 1 to 1; if True - all the Galactic DWDs are represented by a smaller number, N, binaries
+               'UseRepresentingWDs': True, #If False - each binary in the Galaxy is drawn as 1 to 1; if True - all the Galactic DWDs are represented by a smaller number, N, binaries
                'RepresentDWDsBy': 500000,  #Represent the present-day LISA candidates by this nubmer of binaries
                'LISAPCutHours': (2/1.e-4)/(3600.), #LISA cut-off orbital period, 1.e-4 Hz + remember that GW frequency is 2X the orbital frequency
                'MaxTDelay': 14000,
@@ -37,7 +37,8 @@ ModelParams = { #Main options
                'UseOneBinOnly': False, #If False - use full model; if True - use just one bin, for visualizations
                'OneBinToUse': 10, #Number of the bin, if only one bin in used
                'NPoints': 1e5, # Number of stars to sample if we just sample present-day stars
-               'datPath': 'path_to_dat' #os.environ['UCB_GOOGLE_DRIVE_DIR'] # replace with path to your data
+               'datPath': 'path_to_dat' # replace with path to your data
+               #'datPath': os.environ['UCB_GOOGLE_DRIVE_DIR'] #use if rclone is set up
     }
 
 # Use argparse to optionally override ModelParams from command line
@@ -103,6 +104,7 @@ try:
 except ValueError as ve:
     print(f"ValueError: {ve}")
 
-gx.create_galaxy(write_path=write_path, verbose=False, write_h5=False)
 if ModelParams['UseRepresentingWDs']:
     gx.create_downsampled_galaxy(write_path=write_path_downsampled, verbose=False, write_h5=False)
+else:
+    gx.create_galaxy(write_path=write_path, verbose=False, write_h5=False)
