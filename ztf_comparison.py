273eda1c576f29833a4c9c82e507e6b83b45ca14
Wouter van Zeist <65263211+wvanzeist@users.noreply.github.com>
2026-03-13 15:10:03 +0100

    ZTF comparison plotting function
    Currently can only plot one galaxy model at a time.

--- ztf_comparison.py
+++ ztf_comparison.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import os
 
 ztf_array = np.genfromtxt('ZTF frequency distance.dat')
@@ -79,6 +80,15 @@ def frequency_distance_bins(code_name, var_type, var_name, rclone_flag=True,
     recalc_rad: bool
         If True, calculate radii based on the wd_radius_PPE() formula. If
         False, use whatever radii are provided in the galaxy file.
+    
+    Returns
+    -------
+    amount_per_bin: array
+        Numbers of binaries per frequency-distance bin.
+    log_freq_bin_bounds: array/list
+        Edges of the bins in log(frequency/Hz).
+    dist_bin_bounds: array/list
+        Edges of the bins in distance (pc).
     """
     
     """ Initialisation """
@@ -200,3 +210,128 @@ def frequency_distance_bins(code_name, var_type, var_name, rclone_flag=True,
           'ecl_weight=False): ' + str(int(sum(sum(amount_per_bin)))))
     
     return amount_per_bin, log_freq_bin_bounds, dist_bin_bounds
+
+def plot_ztf_comparison(code_name, var_type, var_name, rclone_flag=True,
+                        ecl_weight=True, recalc_rad=True, model_array=None,
+                        panel_option='one'):
+    """
+    Makes plots comparing a given model galaxy to the ZTF sample. Has the
+    option to plot either one panel from 0 to 2000 pc, or three panels with
+    0-500, 500-1000 and 1000-2000 pc respectively.
+    Note: Currently can plot only one galaxy model at a time.
+    
+    Parameters
+    ----------
+    code_name: str
+        Name of the code (e.g. "ComBinE", "SEVN").
+    var_type: str
+        Whether you want to use the initial condition variations or the mass
+        transfer variations.
+    var_name: str
+        Name of the initial condition/mass transfer variation (e.g.
+        "fiducial").
+    rclone_flag: bool
+        Whether you have set up rclone for the filepaths in the Google Drive or
+        not.
+    ecl_weight: bool
+        If True, weight systems by their eclipse probability when binning them.
+    recalc_rad: bool
+        If True, calculate radii based on the wd_radius_PPE() formula. If
+        False, use whatever radii are provided in the galaxy file.
+    model_array: array
+        Numbers of binaries per frequency-distance bin, as output by
+        frequency_distance_bins(). Overrides the code/variation parameters if
+        specified.
+    panel_option: str
+        Specify "one" or "merged" to have one panel with distances from 0 to
+        2000 pc. Specify "three" or "split" to have separate panels for 0-500,
+        500-1000 and 1000-2000 pc.
+    """
+    
+    if model_array is None:
+        model_array, log_freq_bin_bounds, dist_bin_bounds = \
+            frequency_distance_bins(code_name, var_type, var_name,
+            rclone_flag=rclone_flag, ecl_weight=ecl_weight, recalc_rad=recalc_rad)
+    
+    log_freq_bin_centres = np.linspace(-4.95,-0.05,num=50)
+    #dist_bin_centres = np.linspace(25,1975,num=40)
+    #would be good to couple these to the bin bounds from frequency_distance_bins()
+    
+    ztf_log_freq = ztf_array[:,1]
+    ztf_dist_pc = ztf_array[:,2]
+    
+    ztf_near = np.zeros((50)) #zeros, not empty
+    ztf_mid = np.zeros((50))
+    ztf_far = np.zeros((50))
+    
+    for i in range(len(ztf_log_freq)):
+        freq_bin = np.floor(10*ztf_log_freq[i]) + 50 #rounds to nearest 0.1,
+        #then adds 50 to map -5.0 (-50) to index 0
+        
+        if ztf_dist_pc[i] <= 500:
+            ztf_near[int(freq_bin)] += 1
+            #add the system to the total for the appropriate frequency and distance bin
+        elif ztf_dist_pc[i] <= 1000:
+            ztf_mid[int(freq_bin)] += 1
+        elif ztf_dist_pc[i] <= 2000:
+            ztf_far[int(freq_bin)] += 1
+    
+    freqs_reversed = list(reversed(log_freq_bin_centres[10:30]))
+    
+    if panel_option == 'three' or panel_option == 'split':
+        model_near = [sum(model_array[i,0:10]) for i in range(model_array.shape[0])] #0-500 pc
+        model_mid = [sum(model_array[i,10:20]) for i in range(model_array.shape[0])] #500-1000 pc
+        model_far = [sum(model_array[i,20:40]) for i in range(model_array.shape[0])] #1000-2000 pc
+        
+        model_near_cu = np.cumsum(list(reversed(model_near[10:30])))
+        model_mid_cu = np.cumsum(list(reversed(model_mid[10:30])))
+        model_far_cu = np.cumsum(list(reversed(model_far[10:30])))
+        
+        ztf_near_cu_uncorr = np.cumsum(list(reversed(ztf_near[10:30])))
+        ztf_mid_cu_uncorr = np.cumsum(list(reversed(ztf_mid[10:30])))
+        ztf_far_cu_uncorr = np.cumsum(list(reversed(ztf_far[10:30])))
+        
+        plt.figure(1)
+        plt.step(freqs_reversed,0.6*model_near_cu,'C0-',marker=11,linewidth=2,where='mid',label='Model')
+        plt.step(freqs_reversed,ztf_near_cu_uncorr,'k--',linewidth=2,where='mid',label='ZTF obs.')
+        plt.yscale('log')
+        plt.legend()
+        plt.xlabel('log(Frequency/Hz)')
+        plt.ylabel('Cumulative amount of systems')
+        plt.title('(A) Distance = 0-500 pc')
+        
+        plt.figure(2)
+        plt.step(freqs_reversed,0.6*model_mid_cu,'C0-',marker=11,linewidth=2,where='mid',label='Model')
+        plt.step(freqs_reversed,ztf_mid_cu_uncorr,'k--',linewidth=2,where='mid',label='ZTF obs.')
+        plt.yscale('log')
+        plt.legend()
+        plt.xlabel('log(Frequency/Hz)')
+        plt.ylabel('Cumulative amount of systems')
+        plt.title('(B) Distance = 500-1000 pc')
+        
+        plt.figure(3)
+        plt.step(freqs_reversed,0.6*model_far_cu,'C0-',marker=11,linewidth=2,where='mid',label='Model')
+        plt.step(freqs_reversed,ztf_far_cu_uncorr,'k--',linewidth=2,where='mid',label='ZTF obs.')
+        plt.yscale('log')
+        plt.legend()
+        plt.xlabel('log(Frequency/Hz)')
+        plt.ylabel('Cumulative amount of systems')
+        plt.title('(C) Distance = 1000-2000 pc')
+        
+    elif panel_option == 'one' or panel_option == 'merged':
+        model_all = [sum(model_array[i,0:40]) for i in range(model_array.shape[0])] #0-2000 pc
+        model_all_cu = np.cumsum(list(reversed(model_all[10:30])))
+        ztf_all = [sum(x) for x in zip(ztf_near,ztf_mid,ztf_far)]
+        ztf_all_cu_uncorr = np.cumsum(list(reversed(ztf_all[10:30])))
+        
+        plt.figure(1)
+        plt.step(freqs_reversed,0.6*model_all_cu,'C0-',marker=11,linewidth=2,where='mid',label='Model')
+        plt.step(freqs_reversed,ztf_all_cu_uncorr,'k--',linewidth=2,where='mid',label='ZTF obs.')
+        plt.yscale('log')
+        plt.legend()
+        plt.xlabel('log(Frequency/Hz)')
+        plt.ylabel('Cumulative amount of systems')
+        plt.title('Distance = 0-2000 pc')
+        
+    else:
+        raise ValueError('Invalid panel_option specified.')
